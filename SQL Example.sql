40b0d15c1cfa9f9de4c1eb3914c069658416edde
unknown <cjksong@ajou.ac.kr>
2021-05-17 23:26:39 +0900

    added: sql문

--- SQL Example.sql
+++ SQL Example.sql
@@ -29,45 +29,79 @@ INSERT INTO GovfcDiv
 VALUES ('1440', '21', '해양수산지부');
 
 
- //3. 부서 삭제 - 수원소방서 삭제 (수원소방서의 부서 코드 = 1137001)
-DELETE 
-FROM Dept
-WHERE PK = '1137001';
+ 
 
 
 
  //사용자 이용 과정 중 필요한 SQL
 
-//1. 메인화면 - 공무원 전체 추천 맛집 BEST 10 인 가게이름, 추천수, 총지출금액
+//1. 메인화면 - 공무원 전체 추천 맛집 BEST 10 인 가게이름, 총지출금액
 
-//2. 검색설정 - 부서구분이 직속기관이고 관서가 수원소방서이고 실국이 소방서일 때 부서이름들은?
+select place_nm, total_amt 
+from bizplace as biz , (select biz_reg_no, sum(expendtr_rsltn_amt) as total_amt
+		        from expendtrexcut
+		        Group by expendtrexcut.biz_reg_no
+		        order by sum(expendtr_rsltn_amt) DESC
+		        limit 10) as top 
+where top.biz_reg_no = biz.biz_reg_no
+order by total_amt desc
 
-//3. 검색결과 - 예산 내역 중에서 모든 연도의 6월에서 8월 사이 수원소방서의 BEST 10인 가게 이름, 추천 수와 총지출금액을 알고 싶어.
+
+//2. 검색설정 - 부서구분의 종류는 어떤 것이 있는가?
+select dept_div_nm
+from deptdiv
+
+//2-2. 검색설정 - 부서구분이 직속기관일때 관서의 종류는?
+select govofc_div_nm
+from deptdiv, govofcdiv
+where deptdiv.dept_div_cd = govofcdiv.dept_div_cd and deptdiv.dept_div_nm = '직속기관' 
+
+//2-3. 검색설정 - 부서구분이 직속기관일때 관서는 수원소방서일때 실국의 종류는 어떤것이 있는가?
+select hgdept_div_nm
+from govofcdiv, hgdeptdiv
+where hgdeptdiv.govofc_div_cd = govofcdiv.govofc_div_cd and govofcdiv.govofc_div_nm = '수원소방서'  
+
+//2-4. 검색설정 - 부서구분이 직속기관이고 관서는 수원소방서이고 실국은 소방서일때 부서의 종류는 어떤것이 있는가?
+select dept_nm
+from dept, hgdeptdiv
+where dept.hgdept_div_cd = hgdeptdiv.hgdept_div_cd and hgdeptdiv.hgdept_div_nm = '소방서'
+
+
+//3. 검색결과 - 예산 내역 중에서 모든 연도의 6월에서 8월 사이 수원소방서의 BEST 10인 가게 이름, 총지출금액을 알고 싶어.
 /*예산 내역중에서 aaa기관(관서)의 BEST TOP n을 알고 싶다.*/
-SELECT cust_div_nm
-FROM ExpendtrExcut
-WHERE ExpendtrExcut.dept_cd_nm =
-                (SELECT dept_cd_nm
-                 FROM Dept
-                 WHERE Dept.hgdept_div_cd =
-                                 (SELECT hgdept_div_cd
-                                  FROM HgdeptDiv
-                                  WHERE hgdept_div_cd.govofc_div_nm =
-                                                  (SELECT govofc_div_cd
-                                                   FROM GovofcDiv
-                                                   WHERE govofc_div_cd.govofc_div_nm = ('aaa기관') ) ) )
+select place_nm, total_amt 
+from bizplace as biz , (SELECT biz_reg_no, sum(expendtr_rsltn_amt) as total_amt
+						FROM ExpendtrExcut
+						WHERE ExpendtrExcut.dept_cd_nm =
+               											 (SELECT dept_cd_nm
+                 											FROM Dept
+                											 WHERE Dept.hgdept_div_cd =
+                                 											(SELECT hgdept_div_cd
+                                 													 FROM HgdeptDiv
+                                 												 WHERE hgdeptdiv.hgdept_div_nm = '경기도인재개발원') and 6<EXTRACT(MONTH from paymnt_command_de) and EXTRACT(MONTH from paymnt_command_de)<8 )
 
-GROUP BY biz_reg_no
-ORDER BY SUM(expendtr_resltn_amt) DESC
-LIMIT n;
+																					GROUP BY biz_reg_no
+																									ORDER BY SUM(expendtr_rsltn_amt) 
+																												limit 10) as top 
+where top.biz_reg_no = biz.biz_reg_no
+order by total_amt desc
 
 
 //4. 추천하기 - 해당 가게 이름 추천 수 갱신
-UPDATE
-FROM UserRecommend
-SET (like_count = like_count+1)
-WHERE biz_reg_no = ?;
+update userrecommend
+set like_count = (like_count+1)
+where biz_reg_no = (SELECT biz_reg_no
+	            from bizplace
+	            where place_nm = '본수원갈비')
 
+
+
+
+//5. 본수원갈비 추천수 가져오기
+select like_count 
+from userrecommend, bizplace
+where userrecommend.biz_reg_no = bizplace.biz_reg_no 
+      and bizplace.place_nm = '본수원갈비'
 
 
 
