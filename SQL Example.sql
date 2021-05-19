45c43d17baf7de8e1c50a51bf7322bdf80279262
pengm2ng <cjksong@ajou.ac.kr>
2021-05-20 01:07:10 +0900

    added: query image

--- SQL Example.sql
+++ SQL Example.sql
@@ -62,21 +62,7 @@ VALUES ('1440', '21', '해양수산지부');
 
 --사용자 이용 과정 중 필요한 SQL
 --1. 메인화면 - 공무원 전체 추천 맛집 BEST 10 인 가게이름, 총지출금액
-select place_nm,
-        total_amt,
-         U.like_account
-from bizplace as biz 
-        left join userrecommend u on biz.biz_reg_no = u.biz_reg_no,
-        (
-                select biz_reg_no,
-                        sum(expendtr_rsltn_amt) as total_amt
-                from expendtrexcut
-                Group by expendtrexcut.biz_reg_no
-                order by sum(expendtr_rsltn_amt) DESC
-                limit 10
-        ) as top
-where top.biz_reg_no = biz.biz_reg_no
-order by total_amt desc;
+
 
 
 
@@ -93,64 +79,33 @@ from deptdiv;
 
 -- 2-2. 검색설정 - 부서구분이 직속기관일때 관서의 종류는?
 select govofc_div_nm
-from deptdiv,
-        govofcdiv
-where deptdiv.dept_div_cd = govofcdiv.dept_div_cd
-        and deptdiv.dept_div_nm = '직속기관';
-
+from deptdiv
+         join govofcdiv g on deptdiv.dept_div_cd = g.dept_div_cd
+where dept_div_nm = '직속기관';
 
 
 
 
 -- 2-3. 검색설정 - 부서구분이 직속기관일때 관서는 수원소방서일때 실국의 종류는 어떤것이 있는가?
+
+
 select hgdept_div_nm
-from govofcdiv,
-        hgdeptdiv
-where hgdeptdiv.govofc_div_cd = govofcdiv.govofc_div_cd
-        and govofcdiv.govofc_div_nm = '수원소방서';
-
-
-
+from govofcdiv
+         join hgdeptdiv h on govofcdiv.govofc_div_cd = h.govofc_div_cd
+where govofcdiv.govofc_div_nm = '부천소방서';
 
 
 -- 2-4. 검색설정 - 부서구분이 직속기관이고 관서는 수원소방서이고 실국은 소방서일때 부서의 종류는 어떤것이 있는가?
 select dept_nm
-from dept,
-        hgdeptdiv
-where dept.hgdept_div_cd = hgdeptdiv.hgdept_div_cd
-        and hgdeptdiv.hgdept_div_nm = '소방서';
+from dept
+         join hgdeptdiv h on h.hgdept_div_cd = dept.hgdept_div_cd
+where hgdept_div_nm = '소방서';
+
+
 -- 3. 검색결과 - 예산 내역 중에서 모든 연도의 6월에서 8월 사이 수원소방서의 BEST 10인 가게 이름, 총지출금액을 알고 싶어.
 /*예산 내역중에서 aaa기관(관서)의 BEST TOP n을 알고 싶다.*/
-SELECT place_nm,
-        total_amt
-FROM bizplace AS biz,
-        (
-                SELECT biz_reg_no,
-                        SUM(expendtr_rsltn_amt) AS total_amt
-                FROM ExpendtrExcut
-                WHERE ExpendtrExcut.dept_cd_nm = (
-                                SELECT dept_cd_nm
-                                FROM Dept
-                                WHERE Dept.hgdept_div_cd = (
-                                                SELECT hgdept_div_cd
-                                                FROM HgdeptDiv
-                                                WHERE hgdeptdiv.hgdept_div_nm = '경기도인재개발원'
-                                        ) 6 < EXTRACT(
-                                                MONTH
-                                                FROM paymnt_command_de
-                                        )
-                                        AND EXTRACT(
-                                                MONTH
-                                                FROM paymnt_command_de
-                                        ) < 8
-                        )
-                GROUP BY biz_reg_no
-                ORDER BY SUM(expendtr_rsltn_amt)
-                LIMIT 10
-        ) AS top
-WHERE top.biz_reg_no = biz.biz_reg_no
-ORDER BY total_amt DESC;
 
+                       ;
 
 -- 4. 추천하기 - 해당 가게 이름 추천 수 갱신
 update userrecommend
@@ -160,40 +115,6 @@ where biz_reg_no = (
                 from bizplace
                 where place_nm = '본수원갈비'
         );
--- 5. 본수원갈비 추천수 가져오기
-select like_count
-from userrecommend,
-        bizplace
-where userrecommend.biz_reg_no = bizplace.biz_reg_no
-        and bizplace.place_nm = '본수원갈비';
-
-
-
-
-SELECT place_nm,
-        total_amt,
-       U.like_account
-FROM bizplace AS biz left join userrecommend u on biz.biz_reg_no = u.biz_reg_no, (
-                        SELECT biz_reg_no, SUM(expendtr_rsltn_amt) AS total_amt
-                        FROM ExpendtrExcut as expend, (
-                                                        SELECT dept_cd_nm
-                                                        FROM Dept as dept2,(
-                                                                        SELECT hgdept_div_cd
-                                                                        FROM HgdeptDiv
-                                                                        WHERE hgdeptdiv.hgdept_div_nm = '소방서'
-                                                                        ) as hgdept
-                                                        WHERE Dept2.hgdept_div_cd = hgdept.hgdept_div_cd
-                                                        ) as dept_cd
-                        WHERE Expend.dept_cd_nm = dept_cd.dept_cd_nm
-                                AND 6 < EXTRACT( MONTH FROM paymnt_command_de )
-                                AND EXTRACT( MONTH FROM paymnt_command_de ) < 8
-                        GROUP BY biz_reg_no
-                        ORDER BY SUM(expendtr_rsltn_amt) DESC
-                        LIMIT 10
-                ) AS top
-WHERE top.biz_reg_no = biz.biz_reg_no
-ORDER BY total_amt DESC;
-
 
 
 
