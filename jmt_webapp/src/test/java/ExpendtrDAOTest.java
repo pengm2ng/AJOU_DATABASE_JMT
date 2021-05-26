8282a8950f42d4352378e80c581cf9b4dc5506ed
Lani Jung <lani009@naver.com>
2021-05-27 04:00:41 +0900

    added testcases of webapp

---
+++ jmt_webapp/src/test/java/ExpendtrDAOTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import dao.impl.ExpendtrExcutDAO;
+
+public class ExpendtrDAOTest {
+    @Test
+    public void getPlaceTopTenTest() {
+        ExpendtrExcutDAO.getInstance()
+    }
+}
