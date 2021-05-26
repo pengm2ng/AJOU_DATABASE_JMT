21593e69ba730bcf54c3e5d1df9125b53fcd5e1d
pengm2ng <cjksong@ajou.ac.kr>
2021-05-26 11:34:42 +0900

    jdbc 추가/오류해결 필요

--- jmt_webapp/src/main/java/jdbc/ConnectionProvider.java
+++ jmt_webapp/src/main/java/jdbc/ConnectionProvider.java
@@ -1,12 +1,20 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionProvider {
     /**
      * JDBC 커넥션 생성 및 제공
      */
     public static Connection getJDBCConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:apache:commons:dbcp:dbdbdev");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- jmt_webapp/src/main/java/jdbc/DBCPInit.java
+++ jmt_webapp/src/main/java/jdbc/DBCPInit.java
@@ -1,6 +1,45 @@
 package jdbc;
 
-public class DBCPInit {
-    dfdf
+import org.postgresql.core.ConnectionFactory;
+
+public class DBCPInit extends HttpServlet{
+    //web.xml 해결해야함.
+
+    private static final long serialVersionUID = 1L;
+
+	public DBCPInit() {
+		super();
+	}
+
+	private void initConnectionPool() {
+
+		try {
+			String jdbcUrl = "";
+			String username = "";
+			String pw = "";
+
+			ConnectionFactory connFactory = new DriverManagerConnectionFactory(jdbcUrl, username, pw);
+			PoolableConnectionFactory poolableConnFactory = new PoolableConnectionFactory(connFactory, null);
+			poolableConnFactory.setValidationQuery("select 1");
+
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setTimeBetweenEvictionRunsMillis(1000L * 60L * 5L);
+			poolConfig.setTestWhileIdle(true);
+			poolConfig.setMinIdle(4);
+			poolConfig.setMaxTotal(50);
+
+			GenericObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<PoolableConnection>(
+					poolableConnFactory, poolConfig);
+			poolableConnFactory.setPool(connectionPool);
+
+			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
+			PoolingDriver driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+			driver.registerPool("dbdbdev", connectionPool);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
     
 }
