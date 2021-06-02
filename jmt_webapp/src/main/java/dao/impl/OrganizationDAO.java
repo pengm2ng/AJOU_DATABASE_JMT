9fc48b39039bbd137a40d2b0cc7db92cc7a3e696
pengm2ng <cjksong@ajou.ac.kr>
2021-06-02 18:12:20 +0900

    add organizationDAO

--- jmt_webapp/src/main/java/dao/impl/OrganizationDAO.java
+++ jmt_webapp/src/main/java/dao/impl/OrganizationDAO.java
@@ -16,6 +16,14 @@ import entity.Organization;
 import jdbc.ConnectionProvider;
 
 public class OrganizationDAO implements OrganizationDAOI {
+    private static final String ORGANIZATION_QUERY = "select dept_div_nm, govofc_div_nm, hgdept_div_nm, dept_nm\n"
+            + "from \"OrganizationChart\"\n"
+            +     "join \"DeptDiv\" DD on DD.dept_div_cd = \"OrganizationChart\".dept_div_cd\n"
+            +     "join \"GovofcDiv\" GD on GD.govofc_div_cd = \"OrganizationChart\".govofc_div_cd\n"
+            +     "join \"HgdeptDiv\" HD on HD.hgdept_div_cd = \"OrganizationChart\".hgdept_div_cd\n"
+            +     "join \"Dept\" D on D.dept_cd_nm = \"OrganizationChart\".dept_cd_nm\n"
+            + "where dept_div_nm like = ?\n" + "and govofc_div_nm like = ?\n" + "and hgdept_div_nm like = ?\n"
+            + "and dept_nm like = ?";
 
     private OrganizationDAO() {
     }
@@ -30,148 +38,120 @@ public class OrganizationDAO implements OrganizationDAOI {
 
     @Override
     public List<Organization> getAllOrganization(Class<Organization> organizationClass) {
-        /**
-         * '부서구분' 카테고리에서 '전체'를 선택했을때, '관서'에 대해서 모든 list를 가져오는 내용
-         */
         if (organizationClass.equals(DeptDiv.class)) {
+            // DeptDiv 부서구분 전체 가져오기
             try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                List<Organization> govofcList = new ArrayList<>();
+                List<Organization> deptDivList = new ArrayList<>();
 
-                PreparedStatement pstmt = conn.prepareStatement("select * from public \"OrganizationChart\"");
+                PreparedStatement pstmt = conn.prepareStatement("select * from \"DeptDiv\"");
                 ResultSet rs = pstmt.executeQuery();
 
                 while (rs.next()) {
-                    String govofcDivCode = rs.getString("Govofc_div_cd");
-                    pstmt = conn
-                            .prepareStatement("select govofc_div_nm from public.\"GovofcDiv\" where govofc_div_cd=?");
-                    pstmt.setString(1, govofcDivCode);
-                    rs = pstmt.executeQuery();
-                    String govofcDivName = rs.getString("Govofc_div_nm");
-                    govofcList.add(new GovofcDiv(govofcDivCode, govofcDivName));
+                    deptDivList.add(new DeptDiv(rs.getString("dept_div_cd"), rs.getString("dept_div_nm")));
                 }
+                pstmt.close();
+                rs.close();
+                return deptDivList;     // TODO 리턴 작성
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        } else if (organizationClass.equals(GovofcDiv.class)) {
-            /**
-             * '관서' 카테고리에서 '전체'를 선택했을때, '실국'에 대해서 모든 list를 가져오는 내용
-             */
-            try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                ArrayList<Organization> hgdeptList = new ArrayList<>();
 
-                PreparedStatement pstmt = conn.prepareStatement("select * from public.\"OrganizationChart\"");
+        } else if (organizationClass.equals(GovofcDiv.class)) {
+            // GovofcDiv 관서 전체 가져오기
+            try (Connection conn = ConnectionProvider.getJDBCConnection()) {
+                List<Organization> govofcDivList = new ArrayList<>();
+
+                PreparedStatement pstmt = conn.prepareStatement("select * from \"GovofcDiv\"");
                 ResultSet rs = pstmt.executeQuery();
 
                 while (rs.next()) {
-                    String hgdeptCode = rs.getString("hgdept_div_cd");
-                    pstmt = conn
-                            .prepareStatement("select hgdept_div_nm from public.\"HgdeptDiv\" where hgdept_div_cd=?");
-                    pstmt.setString(1, hgdeptCode);
-                    rs = pstmt.executeQuery();
-                    String hgdeptName = rs.getString("hgdept_div_nm");
-                    hgdeptList.add(new HgdeptDiv(hgdeptCode, hgdeptName));
+                    govofcDivList.add(new GovofcDiv(rs.getString("govofc_div_cd"), rs.getString("govofc_div_nm")));
                 }
+                pstmt.close();
+                rs.close();
+                return govofcDivList;     // TODO 리턴 작성
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         } else if (organizationClass.equals(HgdeptDiv.class)) {
-            /**
-             * '실국' 카테고리에서 '전체'를 선택했을때, '부서'에 대해서 모든 list를 가져오는 내용
-             */
+            // HgdeptDiv 실국 전체 가져오기
             try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                ArrayList<Organization> deptList = new ArrayList<>();
+                List<Organization> hgdeptDivList = new ArrayList<>();
 
-                PreparedStatement pstmt = conn.prepareStatement("select * from public.\"OrganizationChart\"");
+                PreparedStatement pstmt = conn.prepareStatement("select * from \"HgdeptDiv\"");
                 ResultSet rs = pstmt.executeQuery();
 
                 while (rs.next()) {
-                    String deptCode = rs.getString("dept_cd_nm");
-                    pstmt = conn.prepareStatement("select dept_nm from public.\"Dept\" where dept_cd_nm=?");
-                    pstmt.setString(1, deptCode);
-                    rs = pstmt.executeQuery();
-                    String deptName = rs.getString("dept_nm");
-                    deptList.add(new Dept(deptCode, deptName));
+                    hgdeptDivList.add(new HgdeptDiv(rs.getString("hgdept_div_cd"), rs.getString("hgdept_div_nm")));
                 }
+                pstmt.close();
+                rs.close();
+                return hgdeptDivList;     // TODO 리턴 작성
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        } else if (organizationClass.equals(Dept.class)) {
+                // Dept 부서 전체 가져오기
+                try (Connection conn = ConnectionProvider.getJDBCConnection()) {
+                    List<Organization> deptList = new ArrayList<>();
+    
+                    PreparedStatement pstmt = conn.prepareStatement("select * from \"Dept\"");
+                    ResultSet rs = pstmt.executeQuery();
+    
+                    while (rs.next()) {
+                        deptList.add(new Dept(rs.getString("dept_cd_nm"), rs.getString("dept_nm")));
+                    }
+                    pstmt.close();
+                    rs.close();
+                    return deptList;     // TODO 리턴 작성
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
         }
         return new ArrayList<>();
     }
 
     @Override
-    public List<Organization> getChildrenOf(Organization organization) {
-        if (organization.getClass().equals(DeptDiv.class)) {
-            /**
-             * '부서구분'카테고리에서 선택할 경우, 해당 '부서구분'에 해당되는 '관서'들의 list를 가져오는 내용
-             */
-            ArrayList<Organization> govofcList = new ArrayList<Organization>();
-            try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                PreparedStatement pstmt = conn.prepareStatement(
-                        "select Govofc_div_cd from public.\"OrganizationChart\" where dept_div_cd=?");
-                pstmt.setString(1, organization.getOrganizationCode());
-                ResultSet rs = pstmt.executeQuery();
+    public List<Organization> getChildrenOf(Organization deptDiv, Organization govofcDiv, Organization hgdeptDiv,
+            Organization dept) {
+        String deptDivName = deptDiv == null ? "%" : deptDiv.getOrganizationName();
+        String govofcDivName = govofcDiv == null ? "%" : govofcDiv.getOrganizationName();
+        String hgdeptDivName = hgdeptDiv == null ? "%" : hgdeptDiv.getOrganizationName();
+        String deptName = dept == null ? "%" : dept.getOrganizationName();
 
-                while (rs.next()) {
-                    String govofcDivCode = rs.getString("Govofc_div_cd");
-                    pstmt = conn
-                            .prepareStatement("select govofc_div_nm from public.\"GovofcDiv\" where govofc_div_cd=?");
-                    pstmt.setString(1, govofcDivCode);
-                    rs = pstmt.executeQuery();
-                    String govofcDivName = rs.getString("Govofc_div_nm");
-                    govofcList.add(new GovofcDiv(govofcDivCode, govofcDivName));
+        List<Organization> children = new ArrayList<>();
+        try (Connection conn = ConnectionProvider.getJDBCConnection();
+                PreparedStatement organizationPstmt = conn.prepareStatement(ORGANIZATION_QUERY);) {
+
+            organizationPstmt.setString(1, deptDivName);
+            organizationPstmt.setString(2, govofcDivName);
+            organizationPstmt.setString(3, hgdeptDivName);
+            organizationPstmt.setString(4, deptName);
+
+            ResultSet rs = organizationPstmt.executeQuery();
+
+            while (rs.next()) {
+                Organization element = null;
+                if (deptDiv == null) {
+                    throw new RuntimeException("Invalid Parameter");
+                } else if (govofcDiv == null) {
+                    element = new GovofcDiv(rs.getString("govofc_div_cd"), rs.getString("govofc_div_nm"));
+                } else if (hgdeptDiv == null) {
+                    element = new HgdeptDiv(rs.getString("hgdpet_div_cd"), rs.getString("hgdept_div_nm"));
+                } else if (dept == null) {
+                    element = new Dept(rs.getString("dept_nm_cd"), rs.getString("dept_nm"));
+                } else {
+                    throw new RuntimeException("Invalid Parameter");
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
+                children.add(element);
             }
-        } else if (organization.getClass().equals(GovofcDiv.class)) {
-            /**
-             * '관서'카테고리에서 선택할 경우, 해당 '관서'에 해당되는 '실국'들의 list를 가져오는 내용
-             */
-            try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                ArrayList<Organization> hgdeptList = new ArrayList<>();
-
-                PreparedStatement pstmt = conn.prepareStatement(
-                        "select hgdept_div_cd from public.\"OrganizationChart\" where govofc_div_cd=?");
-                pstmt.setString(1, organization.getOrganizationCode());
-                ResultSet rs = pstmt.executeQuery();
-
-                while (rs.next()) {
-                    String hgdeptCode = rs.getString("hgdept_div_cd");
-                    pstmt = conn
-                            .prepareStatement("select hgdept_div_nm from public.\"HgdeptDiv\" where hgdept_div_cd=?");
-                    pstmt.setString(1, hgdeptCode);
-                    rs = pstmt.executeQuery();
-                    String hgdeptName = rs.getString("hgdept_div_nm");
-                    hgdeptList.add(new HgdeptDiv(hgdeptCode, hgdeptName));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else if (organization.getClass().equals(HgdeptDiv.class)) {
-            /**
-             * '실국'카테고리에서 선택할 경우, 해당 '실국'에 해당되는 '부서'들의 list를 가져오는 내용
-             */
-            try (Connection conn = ConnectionProvider.getJDBCConnection()) {
-                ArrayList<Organization> deptList = new ArrayList<>();
-
-                PreparedStatement pstmt = conn.prepareStatement(
-                        "select dept_cd_nm from public.\"OrganizationChart\" where hgdept_div_cd=?");
-                ResultSet rs = pstmt.executeQuery();
-
-                while (rs.next()) {
-                    String deptCode = rs.getString("dept_cd_nm");
-                    pstmt = conn.prepareStatement("select dept_nm from public.\"Dept\" where dept_cd_nm=?");
-                    pstmt.setString(1, deptCode);
-                    rs = pstmt.executeQuery();
-                    String deptName = rs.getString("dept_nm");
-                    deptList.add(new Dept(deptCode, deptName));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            rs.close();
+            return children;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return new ArrayList<>();
     }
+
 
 }
